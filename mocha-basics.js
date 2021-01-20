356c2be9753c3ef4a9b2d104e67fbcdf0063f539
unknown <ricardo.j.galdamez@gmail.com>
2021-01-20 12:01:13 -0600

    Reviewing mocha framework

--- mocha-basics.js
+++ mocha-basics.js
@@ -1,7 +1,23 @@
 //Test suite
-describe("Some Feature", function () {
+describe('Some Feature', function () {
   //Test case
-  it("should do something", function () {
-    console.log("Hello World.");
+
+  //beforeEach for individual tests
+  //afterEach for individual tests
+  before(function () {
+    console.log('Before all test cases');
+  });
+  after(function () {
+    console.log('after all test cases');
+  });
+
+  //Test case 1
+  it('Should show error when missing email', function () {
+    console.log('This is test one');
+  });
+
+  //Test case 2
+  it('Should create an account successfully', function () {
+    console.log('This is test 2');
   });
 });
